2fb0b33e0f456031b8e034c7ab41da664b28e07f
Victor Muraki <vmuraki@MKMONDIAL.local>
2022-05-09 10:23:38 -0300

    Desafio finalizado e ajustado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
 
     if (!newTaskTitle) return;
 
@@ -24,22 +23,17 @@ export function TaskList() {
       title: newTaskTitle,
       isComplete: false,
     }
-
     setTasks(oldTask => [...oldTask, newTask]);
     setNewTaskTitle('');
-
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-
     const newTask = tasks.map(task => task.id == id ? {
       ...task,
       isComplete: !task.isComplete
     } : task)
 
     setTasks(newTask)
-
   }
 
   function handleRemoveTask(id: number) {
